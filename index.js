6660c806ed2ffa972a171056a810e42c6b465fea
nusratnova5 <nusrat.nova16@gmail.com>
2022-11-10 23:25:08 +0600

    mongoDB connected

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const express=require('express');
 const app = express();
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
 const port= process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+//console.log(process.env.DB_USER)
+//console.log(process.env.DB_PASSWORD)
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.cifvw1u.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.get('/',(req,res)=>{
     res.send('Services API Running');
 });
 
 app.listen(port,()=>{
-    console.log(`Courses server running on port,${port}`);
+    console.log(`Services server running on port,${port}`);
 });
