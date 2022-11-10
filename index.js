24d53fb6eb54f4e108aa670d365f8e675d756a84
nusratnova5 <nusrat.nova16@gmail.com>
2022-11-10 23:45:23 +0600

    data inserted in mongoDB

--- index.js
+++ index.js
@@ -15,11 +15,17 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.cifvw1u.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run(){
+    try{
+        const serviceCollection = client.db('photoGallery').collection('services');
+    }
+    finally{
+
+    }
+}
+
+run().catch(err=>console.error(err));
 
 
 app.get('/',(req,res)=>{
