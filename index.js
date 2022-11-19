14aa2d7deb1876acf87424756194fdca82b60639
nusratnova5 <nusrat.nova16@gmail.com>
2022-11-19 14:36:44 +0600

    review add api created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const port= process.env.PORT || 5000;
@@ -19,6 +19,28 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try{
         const serviceCollection = client.db('photoGallery').collection('services');
+        const reviewCollection = client.db('photoGallery').collection('reviews');
+    
+        app.get('/services', async (req,res)=>{
+            const query ={}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+        app.get('/services/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        })
+
+        /* Review */
+        app.post('/reviews', async(req, res)=>{
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            res.send(result);
+        })
     }
     finally{
 
